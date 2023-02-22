892eec21b781f8d1147cd8fe114fff263e5b8519
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-02-22 17:20:09 +0700

    functional component

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 export default class Counter extends Component {
     constructor(props) {
---
+++ src/CounterHooks.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const CounterHooks = ({ initialCount }) => {
+    const [count, setCount] = useState(initialCount);
+    return (
+        <div>
+            <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+            <span>{count}</span>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+        </div>
+    )
+}
+
+export default CounterHooks;
